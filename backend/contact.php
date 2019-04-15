7bfa1478ba5b7b485471efaf326611296b89cf76
Sammy Hecht <srh2kq@virginia.edu>
2019-04-15 17:41:07 -0400

    minor changes

--- backend/contact.php
+++ backend/contact.php
@@ -2,17 +2,22 @@
 
 session_start();
 function emailDevelopers(){
+  // check the request method
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // get form inputs
     $first_name = $_POST["name"];
     $last_name = $_POST["surname"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $full_email = $first_name . " " . $last_name . "\n" . message . "\n";
+    $full_email = $first_name . " " . $last_name . "\n" . $message . "\n";
     $full_email = $full_email . "\n" . "From " . $email;
 
     $full_email = wordwrap($full_email, 70);
 
+
+    // send email
     mail("srh2kq@virginia.edu", "CS4640 Contact", $full_email);
 
     return;
@@ -22,6 +27,10 @@ function emailDevelopers(){
 }
 
 
+
 emailDevelopers();
+
+header('location: ' . "../views/about.html");
+
 
 ?>
