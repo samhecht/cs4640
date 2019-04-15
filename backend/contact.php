11891138ff07e77d127d1aa5f868ca52596f46b5
Johnny <johnnychoi9@gmail.com>
2019-04-15 18:54:09 -0400

    contact form input check

--- backend/contact.php
+++ backend/contact.php
@@ -4,25 +4,26 @@ session_start();
 function emailDevelopers(){
   // check the request method
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (!empty($_POST['name']) && !empty($_POST['surname'])&& !empty($_POST['email'])&& !empty($_POST['message'])) {
+      // get form inputs
+      $first_name = $_POST["name"];
+      $last_name = $_POST["surname"];
+      $email = $_POST["email"];
+      $message = $_POST["message"];
 
-    // get form inputs
-    $first_name = $_POST["name"];
-    $last_name = $_POST["surname"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+      $full_email = $first_name . " " . $last_name . "\n" . $message . "\n";
+      $full_email = $full_email . "\n" . "From " . $email;
 
-    $full_email = $first_name . " " . $last_name . "\n" . $message . "\n";
-    $full_email = $full_email . "\n" . "From " . $email;
-
-    $full_email = wordwrap($full_email, 70);
+      $full_email = wordwrap($full_email, 70);
 
 
-    // send email
-    mail("srh2kq@virginia.edu", "CS4640 Contact", $full_email);
+      // send email
+      mail("srh2kq@virginia.edu", "CS4640 Contact", $full_email);
 
-    return;
-  } else {
-    return;
+      return;
+    } else {
+      return;
+    }
   }
 }
 
