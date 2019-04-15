e47f2aee3f783d97f9e35e30175f6d34b0b82f80
Sammy Hecht <srh2kq@virginia.edu>
2019-04-15 15:35:16 -0400

    contact form

---
+++ backend/contact.php
@@ -0,0 +1,16 @@
+function emailDevelopers(){
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $first_name = $_POST["form_name"];
+    $last_name = $_POST["form_lastname"];
+    $email = $_POST["form_email"];
+    $message = $_POST["form_message"];
+
+    $full_email = $first_name . " " . $last_name . "\n" . message . "\n";
+    $full_email = $full_email . "\n" . "From " . $email;
+
+    mail("srh2kq@virginia.edu", "CS4640 Contact", $full_email);
+    return;
+  } else {
+    return;
+  }
+}
