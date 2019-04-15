4ec82f57c0389090e145f8f43d47a9c8e80fd071
Sammy Hecht <srh2kq@virginia.edu>
2019-04-15 16:24:49 -0400

    fixed email

--- backend/contact.php
+++ backend/contact.php
@@ -3,10 +3,10 @@
 session_start();
 function emailDevelopers(){
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $first_name = $_POST["form_name"];
-    $last_name = $_POST["form_lastname"];
-    $email = $_POST["form_email"];
-    $message = $_POST["form_message"];
+    $first_name = $_POST["name"];
+    $last_name = $_POST["surname"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
     $full_email = $first_name . " " . $last_name . "\n" . message . "\n";
     $full_email = $full_email . "\n" . "From " . $email;
@@ -14,10 +14,14 @@ function emailDevelopers(){
     $full_email = wordwrap($full_email, 70);
 
     mail("srh2kq@virginia.edu", "CS4640 Contact", $full_email);
+
     return;
   } else {
     return;
   }
 }
+
+
+emailDevelopers();
 
 ?>
