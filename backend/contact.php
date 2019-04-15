39303b64dec82ee81722b1ffef9dee95ee4569b9
Sammy Hecht <srh2kq@virginia.edu>
2019-04-15 19:07:42 -0400

    email issues

--- backend/contact.php
+++ backend/contact.php
@@ -1,3 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="../styles/mystyle.css">
+
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+
+    <title>Message Status</title>
+    <style>
+      /* Style the input field */
+      #myInput {
+        padding: 20px;
+        margin-top: -6px;
+        border: 0;
+        border-radius: 0;
+        background: #f1f1f1;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <!-- nav bar -->
+      <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+
+        <a class="navbar-brand" href="./sample.html">Random Movie Generator</a>
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./sample.html">Main</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./recently-released.html">Recently Released</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./user.html">User</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Log In Forms/Button -->
+        <form class="form-inline">
+          <div class="form-group">
+            <label class="sr-only" for="email">Email:</label>
+            <input type="email" class ="form-control" id="email" placeholder="Email" autofocus>
+          </div>
+          <div class="form-group">
+            <label class="sr-only" for="pwd" >Password:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Password">
+          </div>
+          <button type="button" class="btn btn-sm btn-primary login-button" id="login-submit">Submit</button>
+          </form>
+          <div>
+              <p id="login-error" style="color: red;"></p>
+          </div>
+        </nav>
+      </header>
 <?php
 
 session_start();
@@ -35,44 +112,53 @@ function validateInput(){
     $email = $_POST["email"];
     $message = $_POST["message"];
 
+
     $bad = false;
 
-    $req_email = array('@', '.');
+
     // check for email and message
     if (strlen($email) < 4){
       $bad = true;
     }
     if (strlen($message) < 1){
+
       $bad = true;
     }
 
-    $email_arr = $email.explode();
-    $email_diff = array_diff($email_arr, $req_email);
+    if (strpos($email, "@") === false){
 
-    if ((!in_array('@', $email_diff)) || (!in_array('.', $email_diff))){
       $bad = true;
     }
-
+    if (strpos($email, ".") === false){
+      
+      $bad = true;
+    }
 
     return $bad;
+  } else {
+    echo "must be a post request";
+    return false;
   }
+
 }
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
   // if the input is good, email the devs
-  if (!validateInput()){
+  if (validateInput() === false){
     emailDevelopers();
+    echo "Message sent.";
   } else {
-    // need to get this on the about page
-    echo "Please completely fill out the form.  Redirecting...";
+
+    echo "Please completely fill out the form.";
 
   }
 }
 
 
-header('location: ' . "../views/about.html");
+
 
 
 ?>
+</html>
