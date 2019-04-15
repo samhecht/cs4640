9c2366bf5839f6e5de57d05e64ea40d91848c135
Sammy Hecht <srh2kq@virginia.edu>
2019-04-15 18:01:20 -0400

    trying to get server input validation

--- backend/contact.php
+++ backend/contact.php
@@ -26,9 +26,51 @@ function emailDevelopers(){
   }
 }
 
+function validateInput(){
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // get form inputs
+    $first_name = $_POST["name"];
+    $last_name = $_POST["surname"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $bad = false;
+
+    $req_email = array('@', '.');
+    // check for email and message
+    if (strlen($email) < 4){
+      $bad = true;
+    }
+    if (strlen($message) < 1){
+      $bad = true;
+    }
+
+    $email_arr = $email.explode();
+    $email_diff = array_diff($email_arr, $req_email);
+
+    if ((!in_array('@', $email_diff)) || (!in_array('.', $email_diff))){
+      $bad = true;
+    }
 
 
-emailDevelopers();
+    return $bad;
+  }
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  // if the input is good, email the devs
+  if (!validateInput()){
+    emailDevelopers();
+  } else {
+    // need to get this on the about page
+    echo "Please completely fill out the form.  Redirecting...";
+
+  }
+}
+
 
 header('location: ' . "../views/about.html");
 
